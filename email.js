2b0f157044b3a08de4b5386d3bc8b91e022f68ad
Jose <puhongwu@live.com>
2018-01-18 23:55:28 +0800

    init

--- email.js
+++ email.js
@@ -1,22 +1,26 @@
 const nodemailer = require('nodemailer');
 const config = require('./config');
 
-const transporter = nodemailer.createTransport({
-    service: '',
-    auth: {
-        user: '',
-        pass: ''
-    }
-})
+const transporter =  nodemailer.createTransport(config.transport)
 
-const sendEmail = (data) => {
-    transporter.sendMail({
-        from: '',
-        to: '',
-        subject: '',
-        text: '',
-        html: '',
+const sendEmail = async (data, newTransportOption) => {
+    let transport = transporter
+
+    if(typeof newTransportOption === 'object') {
+        transport = newTransportOption
+    }
+
+    transport.sendMail({
+        from: data.from || '',
+        to: data.to || '',
+        subject: data.subject || '',
+        text: data.text || '',
+        html: data.html || '',
         attachments: [{'filename': '', 'content': ''}]
+    }, function(err, msg) {
+        if(err) {
+            console.log(err);
+        }
     })
 };
 
